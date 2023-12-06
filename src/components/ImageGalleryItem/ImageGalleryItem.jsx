71747dd0c129b8e746a42905815395f702df3b54
OliaMedinska <106543799+OliaMedinska@users.noreply.github.com>
2023-12-06 11:36:32 +0100

    refactoring - ImageGalleryItem

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,14 +1,9 @@
-import { Component } from 'react';
 import { GalleryItem, GalleryImg } from './ImageGalleryItem.styled';
 
-export class ImageGalleryItem extends Component {
-  render() {
-    const { imgWeb, name, onImageClick } = this.props;
-
+export const ImageGalleryItem = ({imgWeb, name, onImageClick}) => {
     return (
       <GalleryItem onClick={onImageClick}>
         <GalleryImg src={imgWeb} alt={name} />
       </GalleryItem>
     );
   }
-}
